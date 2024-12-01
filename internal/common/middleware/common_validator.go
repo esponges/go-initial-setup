60bab3f3880015b3fbdce2ff8cc8b0b559f53a19
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-12-01 16:38:20 -0600

    add context

--- internal/common/middleware/common_validator.go
+++ internal/common/middleware/common_validator.go
@@ -2,9 +2,11 @@ package common_validator
 
 import "net/http"
 
+// This is necessary because Go requires that middleware functions be methods on a struct in order to be used as handlers.
+// If we just invoked common_validator.Validate directly,
+// it would not work because Validate is not a standalone function that can be used as a handler.
 type CommonValidator struct{}
 
-// mock middleware validator
 func (c *CommonValidator) Validate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// if the POST request has an unexpected header, return an error

06c3e32400f301e3d58488bb1510796fea1dfda2
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-12-02 12:33:00 -0600

    register handlers

--- internal/configuration.go
+++ internal/configuration.go
@@ -1,11 +1,30 @@
 package internal
 
+import (
+	"github.com/esponges/initial-setup/internal/handlers"
+	"github.com/go-playground/validator/v10"
+)
+
 type Configuration struct {
 	Port string
+	API  API
+}
+
+type API struct {
+	samplePostRequestHandler handlers.SamplePostRequestHandler
 }
 
 func NewConfiguration() *Configuration {
+	// Register validators
+	validate := validator.New()
+
+	// Register handlers
+	samplePostRequestHandler := handlers.NewSamplePostRequestHandler(validate)
+
 	return &Configuration{
 		Port: "8080",
+		API: API{
+			samplePostRequestHandler: *samplePostRequestHandler,
+		},
 	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,18 +5,39 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type Handler struct {
+	HomeHandler        func(w http.ResponseWriter, r *http.Request)
 	HealthCheckHandler func(w http.ResponseWriter, r *http.Request)
 }
 
+// TODO: handlers should be moved to their own packages
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println(time.Now().Date())
 	fmt.Fprintf(w, "Welcome to My Project!")
 }
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+type SamplePostRequestHandler struct {
+	validator *validator.Validate
+}
+
+func NewSamplePostRequestHandler(validator *validator.Validate) *SamplePostRequestHandler {
+	return &SamplePostRequestHandler{
+		validator: validator,
+	}
+}
+
+func (s *SamplePostRequestHandler) samplePostRequestHandler(w http.ResponseWriter, r *http.Request) {
+	// todo: impl validation
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }

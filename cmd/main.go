435d5ed26ca11e97f9d03c462e5210502bd2d617
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-12-04 18:06:32 -0600

    add env vars

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/esponges/initial-setup/internal/router"
 )
 
 func main() {
+	// add env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Now you can access your environment secrets using os.Getenv()
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	apiKey := os.Getenv("API_KEY")
+
+	log.Printf("DB_USERNAME: %s\n", dbUsername)
+	log.Printf("DB_PASSWORD: %s\n", dbPassword)
+	log.Printf("API_KEY: %s\n", apiKey)
+
 	// Initialize router
 	r := router.SetupRouter()
 

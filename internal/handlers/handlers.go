af90c0b1ac271d8fea709ac235741c7aa95e7de3
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-12-07 17:30:00 -0600

    insert hardcoded, todo insert from req

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"cloud.google.com/go/spanner"
 	"github.com/esponges/initial-setup/internal/common"
 	"github.com/esponges/initial-setup/internal/handlers/create_singers_handler"
 	"github.com/esponges/initial-setup/internal/handlers/sample_post_request"
@@ -71,6 +74,57 @@ func (c *CreateSingersHandlerImpl) CreateSingersHandler(w http.ResponseWriter, r
 		w.Write([]byte("Bad Request"))
 	} else {
 		log.Println("Correct Request")
+
+		ctx := context.Background()
+		client, err := spanner.NewClient(ctx, os.Getenv("SPANNER_DB"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Close()
+
+		_, err = client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+			// sql := "INSERT Singers (SingerId, FirstName, LastName) VALUES (:singerId, :firstName, :lastName)"
+
+			// none of these work
+			// stmt := spanner.Statement{
+			// 	SQL: sql,
+			// 	Params: map[string]interface{}{
+			// 		"singerId":  "13",
+			// 		"firstName": "foo",
+			// 		"lastName":  "bar",
+			// 	},
+			// }
+			log.Println(string(req.Name), string(req.LastName))
+
+			sql := fmt.Sprintf("INSERT Singers (SingerId, FirstName, LastName) VALUES ('%s', '%s', '%s')",
+				req.SingerId, req.Name, req.LastName)
+
+			stmt := spanner.Statement{
+				SQL: sql,
+			}
+
+			// this sample code works
+			// stmt := spanner.Statement{
+			// 	SQL: `INSERT Singers (SingerId, FirstName, LastName) VALUES
+			// 		(12, 'Melissa', 'Garcia'),
+			// 		(13, 'Russell', 'Morales'),
+			// 		(14, 'Jacqueline', 'Long'),
+			// 		(15, 'Dylan', 'Shaw')`,
+			// }
+
+			rowCount, err := txn.Update(ctx, stmt)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%d rows inserted.\n", rowCount)
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Write(body)
 	}
